1f05a00d2f3ec0af8aa8827f899e1ccb4cba865d
DeboraEsposito <85322183+DeboraEsposito@users.noreply.github.com>
2022-01-22 22:29:43 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,44 +2,48 @@ botaoCriptografar = document.querySelector('#btn-cripto');
 botaoDescriptografar = document.querySelector('#btn-descripto')
 botaoCopiar = document.querySelector('#btn-copy')
 
-var textoCriptografado = '';
-var textoCapturado = '';
 
 /*Função Criptografar*/
 
 botaoCriptografar.addEventListener("click", function criptografar(event){
 
   event.preventDefault();
-  
+ 
   form = document.querySelector("#form-entrada");
-  var textoCapturado = form.inputTexto.value;    
-   
-    textoCapturado = textoCapturado.replaceAll('e','enter'); 
+  var textoCapturado = form.inputTexto.value;
+  
+    textoCapturado = textoCapturado.replaceAll('e','enter');    
     textoCapturado = textoCapturado.replaceAll('i', 'imes');
     textoCapturado = textoCapturado.replaceAll('a','ai'); 
     textoCapturado = textoCapturado.replaceAll('o','ober'); 
     textoCapturado = textoCapturado.replaceAll('u','ufat');
-    document.querySelector('#msg').value =textoCapturado;          
+    textoCapturado = textoCapturado.replaceAll(/[0-9]/g, numeroInvalido);
       
+    
+    document.querySelector('#msg').value = textoCapturado;   
+
   form.reset();
+  
 })
 
 /*função Descriptografar*/
-botaoDescriptografar.addEventListener("click", function descriptografar(event){
 
-  event.preventDefault();
- 
-  form = document.querySelector("#form-entrada");
-  var textoCriptografado = form.inputTexto.value;   
-  
-    textoCriptografado = textoCriptografado.replaceAll('enter','e'); 
-    textoCriptografado = textoCriptografado.replaceAll('imes', 'i');
-    textoCriptografado = textoCriptografado.replaceAll('ai','a'); 
-    textoCriptografado = textoCriptografado.replaceAll('ober','o'); 
-    textoCriptografado = textoCriptografado.replaceAll('ufat','u'); 
-    document.querySelector('#msg').value =textoCriptografado;
- 
-})
+  botaoDescriptografar.addEventListener("click", function descriptografar(event){
+
+    event.preventDefault();
+    
+    form = document.querySelector("#form-entrada");
+    var textoCriptografado = form.inputTexto.value;    
+      textoCriptografado = textoCriptografado.replaceAll('enter','e'); 
+      textoCriptografado = textoCriptografado.replaceAll('imes', 'i');
+      textoCriptografado = textoCriptografado.replaceAll('ai','a');
+      textoCriptografado = textoCriptografado.replaceAll('ober','o'); 
+      textoCriptografado = textoCriptografado.replaceAll('ufat','u'); 
+      document.querySelector('#msg').value = textoCriptografado;
+
+    form.reset();
+
+  })
 
 /*Função Copiar*/
 
@@ -47,22 +51,14 @@ botaoCopiar.addEventListener("click", function copiar(){
   navigator.clipboard.writeText(document.querySelector('#msg').value); 
     
 })
-/* Regras Codificador: 
-"e" é convertido para "enter" 
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação   
-*/
 
-/* Regras Decodificador: 
-"enter" é convertido para "e" 
-"imes" é convertido para "i"
-"ai" é convertido para "a"
-"ober" é convertido para "o"
-"ufat" é convertido para "u"
-Apenas letras minúsculas
-Não permite acentuação     
-*/
+/*Função não aceita numeros*/
+
+function numeroInvalido(){
+  
+  alert('Não são aceitos número')
+
+  return(' Inválido ')
+}
+
+/*Função não aceita letras maiúsculas*/
